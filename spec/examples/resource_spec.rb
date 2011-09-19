7cd804fa3cd9b86882a0acb3a2ef8c60a83f771b
John Crepezzi <john.crepezzi@gmail.com>
2011-09-19 09:31:29 -0400

    Added a test for userIp

---
+++ spec/examples/resource_spec.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe GooglePlus::Resource do
+
+  it 'should convert user_ip into userIp' do
+    RestClient.should_receive(:get).with(anything, hash_including({ :params => hash_including({ :userIp => 'ip' })}))
+    MockResource.make_request(:get, 'path', :user_ip => 'ip')
+  end
+
+end
+
+class MockResource
+  extend GooglePlus::Resource
+end
